3793f39033f7d4adfe485410f7ad11e5cc3dffbb
Pedro Hos <pedro-hos@outlook.com>
2023-01-23 10:17:52 -0300

    update para agenda lula

--- src/main/java/org/sjcdigital/services/Startup.java
+++ src/main/java/org/sjcdigital/services/Startup.java
@@ -3,17 +3,9 @@
  */
 package org.sjcdigital.services;
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-
-import org.sjcdigital.model.Agenda;
-import org.sjcdigital.utils.ParserUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,26 +38,24 @@ public class Startup {
 		LOGGER.info("The application is starting...");
 
 	
-		try {
-
-			LocalDate start = LocalDate.of(2019, 01, 01);
-			List<Agenda> agendas = new ArrayList<>();
-
-			while (!LocalDate.now().plusDays(1).isEqual(start)) {
-				LOGGER.info("Buscando informações: " + start);
-				agendas.add(scrapper.extraiDadosDoDia(start.format(ParserUtils.DATA_PATTERN_YYYY_MM_DD)));
-				start = start.plusDays(1);
-			}
-
-			LOGGER.info("Total entradas >>>> " + agendas.size());
-			csvParser.convertAndSaveData(agendas);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		summarize.geraRelatorio();
-		joinData.join();
+		/*
+		 * try {
+		 * 
+		 * LocalDate start = LocalDate.of(2019, 01, 01); List<Agenda> agendas = new
+		 * ArrayList<>();
+		 * 
+		 * while (!LocalDate.now().plusDays(1).isEqual(start)) {
+		 * LOGGER.info("Buscando informações: " + start);
+		 * agendas.add(scrapper.extraiDadosDoDia(start.format(ParserUtils.
+		 * DATA_PATTERN_YYYY_MM_DD))); start = start.plusDays(1); }
+		 * 
+		 * LOGGER.info("Total entradas >>>> " + agendas.size());
+		 * csvParser.convertAndSaveData(agendas);
+		 * 
+		 * } catch (IOException e) { e.printStackTrace(); }
+		 * 
+		 * summarize.geraRelatorio(); joinData.join();
+		 */
 
 	}
 
